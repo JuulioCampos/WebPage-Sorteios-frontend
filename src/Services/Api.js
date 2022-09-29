fc5136f3422cf67b9a7497003e219f3fd4482b6d
Julio Campos <julio_neto@icloud.com>
2022-09-29 16:38:07 -0300

    ajuste host

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const BASEURL = "https://8102-2804-431-c7cb-f036-f194-de44-e247-be51.ngrok.io/";
-export const HOST = '8102-2804-431-c7cb-f036-f194-de44-e247-be51.ngrok.io/';
+export const BASEURL = "https://2936-2804-431-c7cb-f036-f194-de44-e247-be51.ngrok.io/";
+export const HOST = '2936-2804-431-c7cb-f036-f194-de44-e247-be51.ngrok.io/';
 
 export const Api = axios.create({
   withCredentials: false,
