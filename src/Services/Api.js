c136968625d41b429476246e23a50028c539aafe
Julio Campos <julio_neto@icloud.com>
2022-10-01 13:07:58 -0300

    heroku url

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const BASEURL = "https://kobe-sorteios-b.herokuapp.com/public/";
-export const HOST = 'kobe-sorteios-b.herokuapp.com/public/';
+export const HOST = 'https://kobe-sorteios-b.herokuapp.com/public/';
 
 export const Api = axios.create({
   withCredentials: false,
