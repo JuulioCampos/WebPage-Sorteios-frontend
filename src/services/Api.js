522a61ddff6c1b9f55fe01fb5fe0cbf86c2b0d50
Julio Campos <39494748+JuulioCampos@users.noreply.github.com>
2022-10-17 17:18:36 -0300

    Update Api.js

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const BASEURL = "https://https://kobe-sorteios-b.herokuapp.com/";
-export const HOST = 'https://https://kobe-sorteios-b.herokuapp.com/';
+export const BASEURL = "https://kobe-sorteios-b.herokuapp.com/";
+export const HOST = 'https://kobe-sorteios-b.herokuapp.com/';
 // export const BASEURL = "http://localhost:8000/";
 // export const HOST = 'http://localhost:8000/';
 export const Api = axios.create({
