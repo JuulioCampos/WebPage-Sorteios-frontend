96887fe3e2833497d6b2ffd640c2a6068e164ad6
julio <julio.neto@icloud.com>
2022-10-18 10:39:47 -0300

    http

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const BASEURL = "http://http://198.199.84.208/";
-export const HOST = 'https://http://198.199.84.208/';
+export const BASEURL = "http://198.199.84.208/";
+export const HOST = 'http://198.199.84.208/';
 // export const BASEURL = "http://localhost:8000/";
 // export const HOST = 'http://localhost:8000/';
 export const Api = axios.create({
