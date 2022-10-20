03dea7015b776456dad023ed91b10a0d29a04785
julio <julio.neto@icloud.com>
2022-10-20 20:12:30 -0300

    erro telefone

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,55 +1,62 @@
-import { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { PRIMARY } from '../../colors/Colors.js';
-import { Input, Navbar, SearchCotas } from './Styles';
-import { ModalWindow } from '../Modal/Modal.jsx';
-import { Button } from '../Button/Index.jsx';
-import { Link } from 'react-router-dom';
-import { maskTelefone, unmaskTelefone } from '../../services/Validations.js';
-import { BuscaCotas } from '../../services/Requests/BuscaCotas.js';
-import Swal from 'sweetalert2';
+import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { PRIMARY } from "../../colors/Colors.js";
+import { Input, Navbar, SearchCotas } from "./Styles";
+import { ModalWindow } from "../Modal/Modal.jsx";
+import { Button } from "../Button/Index.jsx";
+import { Link } from "react-router-dom";
+import { maskTelefone, unmaskTelefone } from "../../services/Validations.js";
+import { BuscaCotas } from "../../services/Requests/BuscaCotas.js";
+import Swal from "sweetalert2";
 
 export const Header = (props) => {
   const [modalVisible, setModalVisible] = useState(false);
-  const [telefone, setTelefone] = useState('')
+  const [telefone, setTelefone] = useState("");
   const [listaCotas, setListaCotas] = useState();
   const [isOpen, setIsOpen] = useState();
-  const data = props.data
+  const data = props.data;
 
   const getCotas = () => {
-    BuscaCotas(unmaskTelefone(telefone))
-      .then(
-        (response) => {
-          // eslint-disable-next-line 
+    if (telefone.length > 10) {
+      BuscaCotas(unmaskTelefone(telefone))
+        .then((response) => {
+          // eslint-disable-next-line
           if (response.status == 200) {
-            setListaCotas(response.data)
-            setModalVisible(true)
-            setIsOpen(true)
-          }
-          else {
+            setListaCotas(response.data);
+            setModalVisible(true);
+            setIsOpen(true);
+          } else {
             Swal.fire({
-              icon: 'error',
-              title: 'Oops...',
-              text: 'Ocorreu um erro interno, mas olhe, você tentar novamente! erro: ECC-01',
-            })
+              icon: "error",
+              title: "Oops...",
+              text: "Ocorreu um erro interno, mas olhe, você tentar novamente! erro: ECC-01",
+            });
           }
-        }
-      ).catch(e=>{
-        Swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: 'Aparentemente você não comprou números. Se isso estiver errado, entre em contato com o suporte!',
         })
-      })
-  }
+        .catch((e) => {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Aparentemente você não comprou números. Se isso estiver errado, entre em contato com o suporte!",
+          });
+        });
+    }
+    else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Você precisa digitar um telefone antes!",
+      });
+    }
+  };
   const btnClose = () => {
-    setModalVisible(false)
-    setIsOpen(false)
-    setListaCotas(null)
-  }
+    setModalVisible(false);
+    setIsOpen(false);
+    setListaCotas(null);
+  };
   return (
     <>
       <Navbar style={{ background: PRIMARY }} variant="dark" expand="lg">
@@ -57,31 +64,43 @@ export const Header = (props) => {
           <Navbar.Brand href="/">LOGODAEMPRESA</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              navbarScroll
-            >
-              <Link className='nav-link' to="/">Página inicial</Link>
+            <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+              <Link className="nav-link" to="/">
+                Página inicial
+              </Link>
 
-              <Link to={'/sorteios'} className="btn-sorteio ">
+              <Link to={"/sorteios"} className="btn-sorteio ">
                 Sorteios
                 <NavDropdown d="navbarScrollingDropdown">
-                  {
-                    data &&
-                    data.map((x, y) =>
-                      <Link key={y} style={{ color: "#533483", fontWeight: "500" }} data-rr-ui-dropdown-item="" className="dropdown-item" tabIndex="0" to={"/sorteio/" + x.id}>
+                  {data &&
+                    data.map((x, y) => (
+                      <Link
+                        key={y}
+                        style={{ color: "#533483", fontWeight: "500" }}
+                        data-rr-ui-dropdown-item=""
+                        className="dropdown-item"
+                        tabIndex="0"
+                        to={"/sorteio/" + x.id}
+                      >
                         {x.titulo}
                       </Link>
-                    )
-                  }
+                    ))}
 
                   <NavDropdown.Divider />
-                  <Link style={{ color: "#89898a", fontWeight: "400" }} data-rr-ui-dropdown-item="" className="dropdown-item" tabIndex="0" to="/sorteio/finalizados">
+                  <Link
+                    style={{ color: "#89898a", fontWeight: "400" }}
+                    data-rr-ui-dropdown-item=""
+                    className="dropdown-item"
+                    tabIndex="0"
+                    to="/sorteio/finalizados"
+                  >
                     Finalizados
                   </Link>
                 </NavDropdown>
               </Link>
-              <Link className='nav-link' to={'/contato'}>Contato</Link>
+              <Link className="nav-link" to={"/contato"}>
+                Contato
+              </Link>
             </Nav>
             <Form className="d-flex">
               <Input
@@ -93,14 +112,24 @@ export const Header = (props) => {
                 value={maskTelefone(telefone)}
               />
               <SearchCotas className="search-cotas">
-                <Button label="Buscar Números" onClick={getCotas}>Buscar Números</Button>
+                <Button label="Buscar Números" onClick={getCotas}>
+                  Buscar Números
+                </Button>
               </SearchCotas>
             </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
-      {isOpen && listaCotas && <ModalWindow visible={modalVisible} btnClose={btnClose} idSorteio={null} telefone={telefone} listaCotas={listaCotas}></ModalWindow>}
+      {isOpen && listaCotas && (
+        <ModalWindow
+          visible={modalVisible}
+          btnClose={btnClose}
+          idSorteio={null}
+          telefone={telefone}
+          listaCotas={listaCotas}
+        ></ModalWindow>
+      )}
     </>
   );
-}
+};
